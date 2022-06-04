f71b529c037f6752d3569ebe5890ff87471bb999
圈圈 <vvvvvvii@yahoo.com.tw>
2022-06-04 11:37:39 +0800

    notFound 切版完成

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,8 +1,21 @@
-function NotFound(){
-  return(
-    <div>
-      <p>出錯了...</p>
-    </div>
-  )
+import styled, { css } from "styled-components";
+
+function NotFound() {
+  return (
+    <Container>
+      <p className="fs-1 fw-bold mb-3">Oops!</p>
+      <p className="fs-2">這個分頁跟你的朋朋一樣揣袂著餒</p>
+    </Container>
+  );
 }
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  background-color: #d7c378;
+`;
+
 export default NotFound;
